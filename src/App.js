6881d9403334d6b3059eb455e1c8f3263a6d8714
Yagnesh <aim01learn@gmail.com>
2021-03-29 22:25:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Person from './Person/Person.js';
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, I am a React App!</h1>
+      <Person />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const Person = () =>{
+return <h2>I am a Person Component!!</h2>;
+}
+export default Person;
